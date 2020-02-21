225337357d7a0118a53b6f52ca3f2f14a6529497
imath <math.viet@gmail.com>
2020-02-21 04:04:54 +0100

    Add a Subscribe to comment feed post action.

--- inc/functions.php
+++ inc/functions.php
@@ -58,3 +58,44 @@ function handbook_label( $label = '', $post_type = '' ) {
 	return $label;
 }
 add_filter( 'handbook_label', __NAMESPACE__ . '\handbook_label', 10, 2 );
+
+/**
+ * Add a Subscribe to comment feed post action.
+ *
+ * @since 1.0.0
+ *
+ * @param array $actions   The o2 post actions.
+ * @param integer $post_id The ID or the post.
+ * @return array           The o2 post actions.
+ */
+function get_o2_post_actions( $actions = array(), $post_id = 0 ) {
+	$actions[26] = array(
+		'action'       => 'follow',
+		'href'         => get_post_comments_feed_link( $post_id ),
+		'classes'      => array( 'subscribe-to-post-feed', 'subscribe-to-feed' ),
+		'rel'          => false,
+		'initialState' => 'default'
+	);
+
+	return $actions;
+}
+add_filter( 'o2_filter_post_actions', __NAMESPACE__ . '\get_o2_post_actions', 10, 2 );
+
+/**
+ * Register a Subscribe to comment feed post action state.
+ *
+ * @since 1.0.0
+ */
+function register_o2_post_action_states() {
+	o2_register_post_action_states( 'follow',
+		array(
+			'default' => array(
+				'shortText' => __( 'Subscribe', 'wptribu-plugin' ),
+				'title'     => __( 'Subscribe to comment feed', 'wptribu-plugin' ),
+				'classes'   => array(),
+				'genericon' => 'genericon-rss'
+			)
+		)
+	);
+}
+add_action( 'init', __NAMESPACE__ . '\register_o2_post_action_states' );
